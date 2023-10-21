728cb1d57f91a876390fa1a9723a402c5420894d
Your Name <you@example.com>
2023-10-21 19:27:57 +0500

    complate config

--- config.js
+++
@@ -1,5 +0,0 @@
-export const PORT = 5555;
-
-export const MongoDB_URL = 'mongodb://127.0.0.1:27017'
-
-
--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import mongoose from "mongoose";
-import { PORT, MongoDB_URL } from "./config.js";
 import express from "express";
 import cors from 'cors'
+import dotenv from 'dotenv'
 import { router } from "./Routes/booksRoute.js";
 
-
+dotenv.config()
 
 const app = express();
 
 app.use(express.json());
 app.use(cors())
-app.use('/books', router)
 
+app.use('/books', router) 
 
+const URL = process.env.MongoDB_URL
+const PORT = process.env.PORT | 5555
 
 mongoose
-  .connect(MongoDB_URL)
+  .connect(URL)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`App is listen to port: ${PORT}`);
